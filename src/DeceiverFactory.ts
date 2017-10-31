5308c83e0f51621d20a19c794237c70c42040526
Andrzej Kopeć <kapkowski@gmail.com>
2017-10-31 23:05:56 +0100

    Extract code for generating fake functions

--- src/DeceiverFactory.ts
+++ src/DeceiverFactory.ts
@@ -6,19 +6,6 @@ export class DeceiverFactory {
     constructor(private mirrorFactory: DeceiverMirrorFactory) {}
 
     public getDeceiver<T, K extends keyof T>(klass: Constructor<T>, mixin: Partial<T> = {}): T {
-        // A little hack to make types happy
-        // tslint:disable-next-line:no-any
-        function getFakeFunc(method: T[K]): any {
-            if (method instanceof Function) {
-                const args = Array(method.length)
-                    .fill(0)
-                    .map(i => `arg${i}`)
-                return new Function(...args, 'return void 0;') // tslint:disable-line:no-function-constructor-with-string-args
-            } else {
-                return method
-            }
-        }
-
         const mirror = this.mirrorFactory(klass)
 
         const methodsPart = mirror
@@ -26,7 +13,7 @@ export class DeceiverFactory {
             .map((name: K) => [name, mirror.getMethod(name)])
             .reduce(
                 (acc: T, [name, method]: [K, T[K]]) => {
-                    acc[name] = getFakeFunc(method)
+                    acc[name] = this.getFakeFunc(method)
 
                     return acc
                 },
@@ -38,5 +25,18 @@ export class DeceiverFactory {
             .reduce((result, name) => ({ ...result, [name]: undefined }), {})
 
         return Object.assign(methodsPart, propertiesPart, mixin)
+    }
+
+    // A little hack to make types happy
+    // tslint:disable-next-line:no-any
+    private getFakeFunc<T, K extends keyof T>(method: T[K]): any {
+        if (method instanceof Function) {
+            const args = Array(method.length)
+                .fill(0)
+                .map(i => `arg${i}`)
+            return new Function(...args, 'return void 0;') // tslint:disable-line:no-function-constructor-with-string-args
+        } else {
+            return method
+        }
     }
 }
